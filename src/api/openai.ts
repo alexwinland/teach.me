3275d7acbdccf3bca8c61c72eb3c7eadada2491c
Alex Rickey Winland <alex.winland@hotmail.com>
2024-09-01 00:15:29 -0300

    Add files via upload

---
+++ src/api/openai.ts
@@ -0,0 +1,25 @@
+import OpenAI from "openai"
+
+type Message = {
+	role: 'user' | 'assistant'
+	content: string
+}
+
+const openai = new OpenAI({
+	apiKey: 'sua-chave-API-aqui',
+	dangerouslyAllowBrowser: true
+})
+
+export async function sendMessage(messages: Message[]) {
+	const response = await openai.chat.completions.create({
+		model: 'gpt-3.5-turbo',
+		messages: messages.map(message => (
+			{ role: message.role, content: message.content }
+		))
+	})
+
+	return {
+		role: response.choices[0].message.role,
+		content: response.choices[0].message.content || ''
+	}
+}
